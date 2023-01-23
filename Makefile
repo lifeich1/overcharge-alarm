2d7463eaadcfd6905a97d5b5bc448493018e148f
lifeich1 <lifeich0@gmail.com>
2023-01-23 19:17:56 +0800

    :white_check_mark: ok simple function

--- Makefile
+++ Makefile
@@ -3,11 +3,12 @@ U := overcharge_alarm@lifeich1.github.io
 
 S := extension.js \
 	 metadata.json \
-	 stylesheet.css
+	 stylesheet.css \
+	 mods/battery.js
 
 SFILE := $(patsubst %,$(U)/%,$(S))
 DFILE := $(patsubst %,$(D)/%,$(SFILE))
-DDIR := $(D)/$(U)
+DDIR := $(D)/$(U) $(D)/$(U)/mods
 
 .PHONY: all
 all: $(DFILE)
@@ -19,7 +20,11 @@ $(DFILE): $(D)/%: %
 $(DDIR):
 	mkdir -p $@
 
-PL_GEA := enable disable
+PL_GEA := enable disable reset
 .PHONY: $(PL_GEA)
 $(PL_GEA):
 	gnome-extensions $@ $(U)
+
+.PHONY: dbg
+dbg:
+	dbus-run-session -- gnome-shell --nested --wayland >shell.log 2>shell.err
--- overcharge_alarm@lifeich1.github.io/extension.js
+++ overcharge_alarm@lifeich1.github.io/extension.js
@@ -21,20 +21,46 @@
 
 const GETTEXT_DOMAIN = 'my-indicator-extension';
 
-const { GObject, St, GLib } = imports.gi;
+const { Gio, GObject, St, GLib } = imports.gi;
 
 const ExtensionUtils = imports.misc.extensionUtils;
 const Main = imports.ui.main;
 const PanelMenu = imports.ui.panelMenu;
 const PopupMenu = imports.ui.popupMenu;
 
-const { Battery } = imports.mods.battery;
+const Me = ExtensionUtils.getCurrentExtension();
+
+const { Battery } = Me.imports.mods.battery;
 
 const _ = ExtensionUtils.gettext;
 
+let _cancel = null;
+let _full_cnt = 0;
+let _cur_status = '';
+
+async function _pull() {
+    _cur_status = await Battery.battery_status(_cancel);
+}
+
 function _do_alarm() {
     Main.notify(_('Warning: battery overcharge'));
+    // TODO custom cmd at prefs
     GLib.spawn_command_line_async('aplay /tmp/tell-overcharge.wav');
+}
+
+function _unless_alarm(i) {
+    return ((i - 1) & i) != 0;
+}
+
+function _check() {
+    if ('Full' == _cur_status) {
+        _full_cnt ++;
+        // TODO alarm cond in prefs
+        if (_unless_alarm(_full_cnt)) return;
+        _do_alarm();
+    } else {
+        _full_cnt = 0;
+    }
 }
 
 const Indicator = GObject.registerClass(
@@ -42,16 +68,31 @@ class Indicator extends PanelMenu.Button {
     _init() {
         super._init(0.0, _('My Shiny Indicator'));
 
-        this.add_child(new St.Icon({
-            icon_name: 'face-smile-symbolic',
-            style_class: 'system-status-icon',
-        }));
+        this._icon = new St.Icon({
+            icon_name: 'battery-level-50-symbolic',
+            style_class: 'overcharge-alarm-icon',
+            fallback_icon_name: 'battery-level-50-symbolic',
+        });
+        this.add_child(this._icon);
 
         let item = new PopupMenu.PopupMenuItem(_('Show Notification'));
         item.connect('activate', () => {
-            _do_alarm();
+            Main.notify(_('battery status: ' + _cur_status + '; cnt ' + _full_cnt));
         });
         this.menu.addMenuItem(item);
+    }
+
+    updateBatStat() {
+        let s = _cur_status;
+        let i = 'battery-missing-symbolic';
+        if ('Full' == s) {
+            i = 'battery-level-100-charging-symbolic';
+        } else if ('Discharging' == s) {
+            i = 'battery-level-60-symbolic';
+        } else if ('Charging' == s) {
+            i = 'battery-level-40-charging-symbolic';
+        }
+        this._icon.set_icon_name(i);
     }
 });
 
@@ -62,14 +103,37 @@ class Extension {
         ExtensionUtils.initTranslations(GETTEXT_DOMAIN);
     }
 
+    _trigger_pull() {
+        _pull().then(() => {
+            this._indicator.updateBatStat();
+            _check();
+        });
+    }
+
     enable() {
+        _full_cnt = 0;
+        _cancel = new Gio.Cancellable();
         this._indicator = new Indicator();
         Main.panel.addToStatusArea(this._uuid, this._indicator);
+
+        // TODO timeout sec add to prefs
+        this._checkTaskId = GLib.timeout_add_seconds(GLib.PRIORITY_DEFAULT_IDLE, 60, () => {
+            this._trigger_pull();
+            return GLib.SOURCE_CONTINUE;
+        });
+
+        this._trigger_pull();
     }
 
     disable() {
+        GLib.Source.remove(this._checkTaskId);
+        this._checkTaskId = null;
+        _cancel.cancel();
         this._indicator.destroy();
         this._indicator = null;
+        _full_cnt = 0;
+        _cancel = null;
+        _cur_status = '';
     }
 }
 
